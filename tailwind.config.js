6072f75d698e76372a08e84c910043e72ae2f090
Sae Inoue <flower.smile120717@gmail.com>
2023-11-22 16:17:59 +0800

    feat(index.html):add tailwindcss design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 module.exports = {
   content: [
-      './*.html'
+      './*.html',
+      './index.html',
+      './about.html',
+      './contact.html',
+      './projects.html',
+      './resume.html',
+      './mywork.html',
   ],
   theme: {
     colors:{
+        ...colors,
       'Byzantine blue':'#334DD1',
       'Jonquil': '#FFCC14',
-},
+    },
+      borderWidth:{
+          DEFAULT: '1px',
+          '0': '0',
+          '2': '2px',
+          '3': '3px',
+          '4': '4px',
+          '6': '6px',
+          '8': '8px',
+          '12':'12px',
+          '16':'16px',
+      },
     extend: {},
   },
   plugins: [],
